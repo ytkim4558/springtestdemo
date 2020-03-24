09774d13ea6cf2a862949e6509543efdf21fe947
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2020-03-24 23:19:41 +0900

    CapturedOutput Test

--- src/test/java/me/facemoktak/springtestdemo/sample/SampleControllerTest.java
+++ src/test/java/me/facemoktak/springtestdemo/sample/SampleControllerTest.java
@@ -2,11 +2,14 @@ package me.facemoktak.springtestdemo.sample;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -19,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith({SpringExtension.class, OutputCaptureExtension.class})
 @WebMvcTest(SampleController.class)
 class SampleControllerTest {
 
@@ -30,10 +33,12 @@ class SampleControllerTest {
     MockMvc mockMvc;
 
     @Test
-    public void hello() throws Exception {
+    public void hello(CapturedOutput capturedOutput) throws Exception {
         when(mockSampleService.getName()).thenReturn("yongtak");
 
         mockMvc.perform(get("/hello"))
                 .andExpect(content().string("hello yongtak"));
+
+        assertThat(capturedOutput).contains("holoman").contains("skip");
     }
 }
